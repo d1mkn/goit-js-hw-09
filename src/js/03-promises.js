55ed178d2b39209b2b785b90c7b76b44b6c816ee
Dmytro Hubin <dimkanishe2099@gmail.com>
2023-02-18 04:17:25 +0200

    3rd-task

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,47 @@
+import Notiflix from 'notiflix';
+
+const refs = {
+  delayEl: document.querySelector('[name=delay]'),
+  stepEl: document.querySelector('[name=step]'),
+  amountEl: document.querySelector('[name=amount]'),
+  form: document.querySelector('.form'),
+};
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+}
+
+refs.form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(e) {
+  e.preventDefault();
+
+  let delay = refs.delayEl.valueAsNumber;
+  const step = refs.stepEl.valueAsNumber;
+  const amount = refs.amountEl.valueAsNumber;
+
+  for (let i = 1; i < amount + 1; i += 1) {
+    createPromise(i, delay)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`
+        );
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(
+          `❌ Rejected promise ${position} in ${delay}ms`
+        );
+      });
+    delay += step;
   }
+  e.currentTarget.reset();
 }
